94acafb17c8e7b10ad74c1045a9437d2c03ab409
Syarif <154748261+Syharipf@users.noreply.github.com>
2025-03-23 17:18:32 +0700

    Implementasi design by contract

--- Program.cs
+++ Program.cs
@@ -2,12 +2,27 @@
 {
     static void Main()
     {
-        Console.Write("Masukkan judul video: ");
-        string judul = Console.ReadLine();
-        SayaTubeVideo video = new SayaTubeVideo(judul);
-        Console.Write("Masukkan jumlah penambahan play count: ");
-        int count = int.Parse(Console.ReadLine());
-        video.IncreasePlayCount(count);
-        video.PrintVideoDetails();
+        try
+        {
+            Console.Write("Masukkan judul video: ");
+            string judul = Console.ReadLine();
+            SayaTubeVideo video = new SayaTubeVideo(judul);
+            video.PrintVideoDetails();
+            Console.Write("\nMasukkan jumlah penambahan play count: ");
+            int count = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nMenguji overflow play count...");
+            Random rand = new Random();
+            int loopLimit = rand.Next(50, 300);
+            for (int i=0; i < loopLimit; i++)
+            {
+                video.IncreasePlayCount(count);
+            }
+
+            video.PrintVideoDetails();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Terjadi kesalahan: " + ex.Message);
+        }
     }
 }
