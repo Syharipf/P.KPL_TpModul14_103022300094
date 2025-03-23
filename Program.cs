d825150181614fcb1387346f7f40f40a2b01b52d
Syarif <154748261+Syharipf@users.noreply.github.com>
2025-03-23 16:58:00 +0700

    Mengimplementasikan input judul dan play count

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Masukkan judul video: ");
+        string judul = Console.ReadLine();
+        SayaTubeVideo video = new SayaTubeVideo(judul);
+        Console.Write("Masukkan jumlah penambahan play count: ");
+        int count = int.Parse(Console.ReadLine());
+        video.IncreasePlayCount(count);
+        video.PrintVideoDetails();
+    }
+}
